1ab8739e0cc3226d9418c10ad349d0d216c1112d
aneeta77 <83082902+aneeta77@users.noreply.github.com>
2021-08-01 09:21:35 -0400

    Update Script1627823580632.groovy
    Updated the comment

--- Scripts/New test case/Script1627823580632.groovy
+++ Scripts/New test case/Script1627823580632.groovy
@@ -17,5 +17,5 @@ import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
 
-WebUI.comment('This is demo comment')
+WebUI.comment('This is demo comment for git')
 
